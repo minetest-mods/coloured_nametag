a8f45c595d8b7f18db186a4bc0eebe3f9af22eac
Sapein <chandler182@gmail.com>
2015-05-19 17:22:38 -0500

    Added in one new command and options for /nametag:
    Changed command: "/nametag" - Now any of the 9 colors can be added as a param, eliminating, if the user wants, the need for the formspec.
    New Command: "/nametag-custom" - Changes color to a custom value. Setup uses the RBG Format.

--- init.lua
+++ init.lua
@@ -1,24 +1,92 @@
 minetest.register_chatcommand("nametag",{
-	params = "",
-	description = "Alows a player to set a coloured nametag!",
+	params = "<New Colour>",
+	description = "Alows a player to set a coloured nametag! This can either have a new colour as a param, or it can be used by itself to bring up a formspec.",
 	privs = {shout = true},
-	func = function (name,params)
-	local player = minetest.get_player_by_name(name)
-		minetest.after(1, function()
-			minetest.show_formspec(name, "name-colours",
-			"size[8.5,4.5]"..
-			"label[0.25,0.5;Please select the colour you want your nametag to be:]"..
-			"button_exit[0.25,1.5;2.5,0.5;green;Green]"..
-			"button_exit[3,1.5;2.5,0.5;blue;Blue]"..
-			"button_exit[5.75,1.5;2.5,0.5;red;Red]"..
-			"button_exit[0.25,2.5;2.5,0.5;purple;Purple]"..
-			"button_exit[3,2.5;2.5,0.5;yellow;Yellow]"..
-			"button_exit[5.75,2.5;2.5,0.5;cyan;Cyan]"..
-			"button_exit[0.25,3.5;2.5,0.5;white;White]"..
-			"button_exit[3,3.5;2.5,0.5;orange;Orange]"..
-			"button_exit[5.75,3.5;2.5,0.5;black;Black]"
-		)
-		end)
+	func = function(user, param)
+		local player = minetest.get_player_by_name(user)
+		param = param:lower()
+		if param == "" then
+			minetest.chat_send_player(user, "Showing formspec...")
+			minetest.after(1, function()
+				minetest.show_formspec(user, "name-colours",
+					"size[8.5,4.5]"..
+					"label[0.25,0.5;Please select the colour you want your nametag to be:]"..
+					"button_exit[0.25,1.5;2.5,0.5;green;Green]"..
+					"button_exit[3,1.5;2.5,0.5;blue;Blue]"..
+					"button_exit[5.75,1.5;2.5,0.5;red;Red]"..
+					"button_exit[0.25,2.5;2.5,0.5;purple;Purple]"..
+					"button_exit[3,2.5;2.5,0.5;yellow;Yellow]"..
+					"button_exit[5.75,2.5;2.5,0.5;cyan;Cyan]"..
+					"button_exit[0.25,3.5;2.5,0.5;white;White]"..
+					"button_exit[3,3.5;2.5,0.5;orange;Orange]"..
+					"button_exit[5.75,3.5;2.5,0.5;black;Black]"
+				)
+			end)
+		elseif param == "green" then
+			player:set_nametag_attributes({
+				color = {r = 0, g = 255, b = 0}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "blue" then
+			player:set_nametag_attributes({
+				color = {r = 0, g = 0, b = 255}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "red" then
+			player:set_nametag_attributes({
+				color = {r = 255, g = 0, b = 0}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "purple" then
+			player:set_nametag_attributes({
+				color = {r = 200, g = 0, b = 200}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "yellow" then
+			player:set_nametag_attributes({
+				color = {r = 255, g = 255, b = 0}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "cyan" then
+			player:set_nametag_attributes({
+				color = {r = 0, g = 255, b = 255}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "white" then
+			player:set_nametag_attributes({
+				color = {r = 255, g = 255, b = 255}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "orange" then
+			player:set_nametag_attributes({
+				color = {r = 255, g = 165, b = 0}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		elseif param == "black" then
+			player:set_nametag_attributes({
+				color = {r = 0, g = 0, b = 0}
+			})
+			minetest.chat_send_player(user, "Your nametag colour has been set to " ..param)
+		else
+			minetest.chat_send_player(user, "Incorrect Color! If you don't know what colours are avalible, just use /nametag without an option after it.")
+		end
+	end
+})
+
+minetest.register_chatcommand("nametag-custom", {
+	params = "<Red Value> <Green Value> <Blue Value>",
+	description = "Change the colour of a nametage to a custom colour (in RGB Format)",
+	func = function(user, param)
+		local found, _, redValue, greenValue, blueValue = param:find("^([^%s]+)%s+(.+)%s+(.+)$")
+		if not found then
+			minetest.chat_send_player(user, "Not enough Arguments!")
+			return
+		end
+		local player = minetest.get_player_by_name(user)
+		player:set_nametag_attributes({
+			color = {r = redValue, g = greenValue, b = blueValue}
+		})
+		minetest.chat_send_player(user, "Your nametag colour has been set to the RBG value of " ..redValue .. ", " .. greenValue .. ", " .. blueValue)
 	end
 })
 
--- readme.md
+++ readme.md
@@ -6,6 +6,10 @@ This mod allows players to set their nametag to a colour of their choice.
 ####Usage and effects:
 The usage of this mod is very simple. Once it has been installed, players on the server can use the /nametag command to bring up a formspec, on which they can select a colour, out of red, green, blue, purple, yellow, cyan, white, orange and black. The player's nametag then gets set to that colour! However, this has to be done on every log in, I still need to find a simple way of preserving the nametag colour between logins.
 
+If you prefer, you can add the colour you want (out of the ones listed above) as a param to the /nametag command, which dispenses with the need to use the formspec.
+
+If you want to get creative, you can use the command /nametag-custom with three values (RGB) between 1 and 255 to set your nametag to any colour you like!
+
 Just to warn you: This mod is totally and utterly useless in singleplayer! (Even with f7, the nametag isn't visible.)
 
 ####ToDo:
@@ -13,6 +17,9 @@ Just to warn you: This mod is totally and utterly useless in singleplayer! (Even
 * Make the mod more customisable.
 * Maybe make the formspec a little more colourful.
 * Maybe make a second page of colours?
+
+###Credits:
+Thanks to Chanku/Sapein for the option of using a param to set the colour, and for the custom colour nametag command.
 
 ###License:
 Everything in this mod is licensed under WTFPL.
