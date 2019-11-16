31e6cefc25c00fd43fd21523a411f53cd5ef519d
SmallJoker <SmallJoker@users.noreply.github.com>
2019-11-16 09:37:25 +0100

    Fix indents. Did not test.

--- init.lua
+++ init.lua
@@ -22,34 +22,30 @@ minetest.register_chatcommand("nametag",{
 	description = "Allows players to colour their nametag. Type /nametag to see available colors",
 	privs = {nametag = true},
 	func = function(user, param)
-		local player = minetest.get_player_by_name(user)
 		param = param:lower()
 		if param == "" then
-			minetest.after(1, function()
-				minetest.show_formspec(user, "name-colours",
-					"size[8.5,4.5]"..
-					"label[0.25,0.5;Choose a color:]"..
-					"button_exit[0.25,1.5;2.5,0.5;green;Green]"..
-					"button_exit[3,1.5;2.5,0.5;blue;Blue]"..
-					"button_exit[5.75,1.5;2.5,0.5;red;Red]"..
-					"button_exit[0.25,2.5;2.5,0.5;purple;Purple]"..
-					"button_exit[3,2.5;2.5,0.5;yellow;Yellow]"..
-					"button_exit[5.75,2.5;2.5,0.5;cyan;Cyan]"..
-					"button_exit[0.25,3.5;2.5,0.5;white;White]"..
-					"button_exit[3,3.5;2.5,0.5;orange;Orange]"..
-					"button_exit[5.75,3.5;2.5,0.5;black;Black]"
-				)
-			end)
+			minetest.after(1, minetest.show_formspec, user, "name-colours",
+				"size[8.5,4.5]"..
+				"label[0.25,0.5;Choose a color:]"..
+				"button_exit[0.25,1.5;2.5,0.5;green;Green]"..
+				"button_exit[3,1.5;2.5,0.5;blue;Blue]"..
+				"button_exit[5.75,1.5;2.5,0.5;red;Red]"..
+				"button_exit[0.25,2.5;2.5,0.5;purple;Purple]"..
+				"button_exit[3,2.5;2.5,0.5;yellow;Yellow]"..
+				"button_exit[5.75,2.5;2.5,0.5;cyan;Cyan]"..
+				"button_exit[0.25,3.5;2.5,0.5;white;White]"..
+				"button_exit[3,3.5;2.5,0.5;orange;Orange]"..
+				"button_exit[5.75,3.5;2.5,0.5;black;Black]"
+			)
 		elseif colors[param] then
+			local player = minetest.get_player_by_name(user)
 			player:set_nametag_attributes({
-			color = colors[param]
-		})
-
-minetest.chat_send_player(user, "Your nametag color has been set to: " .. param)
-	else
-		minetest.chat_send_player(user, "Invalid usage, see /help nametag")
+				color = colors[param]
+			})
+			return true, "Your nametag color has been set to: " .. param
+		end
+		return false, "Invalid usage, see /help nametag"
 	end
-end
 })
 
 -- Customized RGB-format color
@@ -60,24 +56,26 @@ minetest.register_chatcommand("custom-nametag", {
 	func = function(user, param)
 		local found, _, redValue, greenValue, blueValue = param:find("^([^%s]+)%s+(.+)%s+(.+)$")
 		if not found then
-			minetest.chat_send_player(user, "Invalid usage, see /help custom-nametag")
-			return
+			return false, "Invalid usage, see /help custom-nametag"
 		end
 		local player = minetest.get_player_by_name(user)
 		player:set_nametag_attributes({
 			color = {r = redValue, g = greenValue, b = blueValue}
 		})
-		minetest.chat_send_player(user, "Your nametag color has been set to the value of: " ..redValue .. ", " .. greenValue .. ", " .. blueValue)
+
+		return true, "Your nametag color has been set to the value of: " ..
+			redValue .. ", " .. greenValue .. ", " .. blueValue
 	end
 })
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "name-colours" then return end
-		for key, value in pairs(colors) do
-			if fields[key] then
-				player:set_nametag_attributes({
-            		color = colors[key]
-				})
+
+	for key, value in pairs(colors) do
+		if fields[key] then
+			player:set_nametag_attributes({
+            			color = colors[key]
+			})
 		end
    	end
 end)
